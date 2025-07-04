31da745d606b3f48c196d8617ac6d86b73b8c2c9
Mahaub Alam <mahabubalom72091@gmail.com>
2025-07-04 16:28:13 +0300

    added some sehcma

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,46 @@
 import express, { Application, NextFunction, Request, Response } from "express";
 
-const app : Application = express();
- 
-app.get("/", (req :Request, res: Response) => {
+const app: Application = express();
+import { model, Schema } from "mongoose";
+
+const noteSchema = new Schema({
+  title: { type: String, require: true, trim: true },
+    content: { type: String, default: "" },
+    category: {
+        type: String,
+        enum: ["personal", "Work", "stduy"],
+        default:"personal"
+    },
+    pinned: {
+        type: Boolean,
+        default:false
+    },
+    tags: {
+        label: { type: String, require: true },
+        color:{type: String, default:"gray"}
+    }
+});
+
+const Note = model("Note", noteSchema);
+app.post("/create-note", async (req: Request, res: Response) => {
+  const myNote = new Note({
+      title: "Learning Expresss",
+      tags: {
+          label: "DatabaseDB"
+      }
+  });
+
+  await myNote.save();
+
+  res.status(201).json({
+    success: true,
+    message: "Note create successfully",
+    note: myNote,
+  });
+});
+
+app.get("/", (req: Request, res: Response) => {
   res.send("Welcome! to not app");
 });
-  
-export default app ;
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,9 @@ const mongoose = require("mongoose");
 async function main() {
     try {
         await mongoose.connect(
-           "mongodb://TodosApp:h34tiiB7SVUOV5rF@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+            "mongodb://TodosApp:h34tiiB7SVUOV5rF@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/Advanced-Note-App?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+            // "mongodb://TodosApp:h34tiiB7SVUOV5rF@ac-cgkxfia-shard-00-00.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-01.x9t7sgg.mongodb.net:27017,ac-cgkxfia-shard-00-02.x9t7sgg.mongodb.net:27017/Advanced-Note-App?ssl=true&replicaSet=atlas-nszs70-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0"
+        
         );
         console.log("Connected to mongoDB")
         server = app.listen(PORT, () => {
